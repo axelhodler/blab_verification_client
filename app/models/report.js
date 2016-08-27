a66f54a36f2683599550067b7ce377e0c4d7a2d8
Axel Hodler <derxor@gmail.com>
2016-08-27 14:07:20 +0200

    can list reports

--- app/models/report.js
+++ app/models/report.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  submitterId: DS.attr('number'),
+  submitterId: DS.attr('string'),
   valid: DS.attr('boolean')
 });
--- app/router.js
+++ app/router.js
@@ -7,7 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('reports');
+  this.route('reports', function() {
+
+  });
 });
 
 export default Router;
