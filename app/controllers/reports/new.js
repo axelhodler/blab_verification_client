6b1c88adf9f22626f8ba7cc50b2746ca1259ffce
Axel Hodler <derxor@gmail.com>
2016-08-28 13:31:27 +0200

    mark input fields as required

--- app/controllers/reports/new.js
+++ app/controllers/reports/new.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  hasHashcode: Ember.computed.notEmpty('model.id'),
+  hasSubmitter: Ember.computed.notEmpty('model.submitterId'),
+  isValid: Ember.computed.and(
+    'hasHashcode',
+    'hasSubmitter'
+  ),
   actions: {
     save() {
       this.get('model').save().then(() => {
