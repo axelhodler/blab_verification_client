c0af7ae0bbebbd16f0ca62f0e940229fb76970ea
Axel Hodler <derxor@gmail.com>
2016-09-01 00:14:46 +0200

    add date input field

--- app/models/report.js
+++ app/models/report.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   submitterId: DS.attr('string'),
   valid: DS.attr('boolean'),
-  verifications: DS.hasMany('verification')
+  verifications: DS.hasMany('verification'),
+  createdOn: DS.attr('date')
 });
