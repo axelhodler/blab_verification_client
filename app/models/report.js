97d686b580b34cbfaa5dee4bffb15124fde0de5f
Axel Hodler <derxor@gmail.com>
2016-09-10 19:38:51 +0200

    extend report model

--- app/models/report.js
+++ app/models/report.js
@@ -4,5 +4,7 @@ export default DS.Model.extend({
   submitterId: DS.attr('string'),
   valid: DS.attr('boolean'),
   verifications: DS.hasMany('verification'),
-  createdOn: DS.attr('date')
+  createdOn: DS.attr('date'),
+  input: DS.attr('number'),
+  output: DS.attr('string')
 });
