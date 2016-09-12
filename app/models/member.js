d127f483076a4ef48f3a03fcb8e39901204e99f1
Axel Hodler <derxor@gmail.com>
2016-09-12 22:09:44 +0200

    extend user obj

--- app/models/member.js
+++ app/models/member.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  verifications: DS.hasMany('verification')
+  verifications: DS.hasMany('verification'),
+  fullName: DS.attr('string'),
+  mail: DS.attr('string'),
+  etherAddress: DS.attr('string')
 });
