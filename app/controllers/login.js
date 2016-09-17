0a224edab8b7c3eb58050ae46ed192ab6a9507df
Axel Hodler <derxor@gmail.com>
2016-09-17 10:51:06 +0200

    hold tokenData central in application controller

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,21 +1,7 @@
 import Ember from 'ember';
 
-const { getOwner } = Ember;
-
 export default Ember.Controller.extend({
   session: Ember.inject.service(),
-
-  tokenData: Ember.computed('session.session.content.authenticated', function() {
-    var authenticator = getOwner(this).lookup('authenticator:jwt'),
-      session = this.get('session.session.content.authenticated'),
-      tokenData = {};
-
-    if(session && Object.keys(session).length > 0) {
-      tokenData = authenticator.getTokenData(session.token);
-    }
-
-    return JSON.stringify(tokenData, null, '\t');
-  }),
 
   actions: {
     authenticate: function() {
